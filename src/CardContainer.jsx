24af470f1a4677c1e55c2bdadff9bf6997257726
nadlumanearjun@gmail.com <nadlumanearjun@gmail.com>
2024-01-20 16:36:29 +0530

    titel

--- src/CardContainer.jsx
+++ src/CardContainer.jsx
@@ -2,30 +2,33 @@ import React from 'react'
 
 const CardContainer = () => {
     return (
-        <div className="p-4">
-            <div className="flex gap-8 flex-wrap justify-evenly">
-                <div className="flex card h-[20rem] w-[15rem] shadow-md rounded-md">
+        <div className="p-4 flex flex-col gap-4 items-center">
+            <div className="h-[3px] w-[10rem] bg-orange-800"/>
+            <span className="text-3xl font-bold">DISCOVERY BALI</span>
+            <span className="text-xl">Adalah sebuah warisan indahnya alam dan budaya yang masih terjaga di Bali yang dapat anda jelajahi</span>
+            <div className="flex gap-2 flex-wrap justify-center">
+                <div className="flex card h-[20rem] w-[20rem] p-4 shadow-md rounded-md">
                     <div className="flex flex-col mt-auto text-white justify-center items-center flex-1 hover:h-[15rem] duration-150 overlay">
                         <span>DESTINATION</span>
                         <div className="h-[3px] bg-orange-500 w-[2rem]"></div>
                         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, incidunt.</span>
                     </div>
                 </div>
-                <div className="flex card h-[20rem] w-[15rem] shadow-md rounded-md">
+                <div className="flex card h-[20rem] w-[20rem] p-4 shadow-md rounded-md">
                     <div className="flex flex-col mt-auto text-white justify-center items-center flex-1 hover:h-[15rem] duration-150">
                         <span>DESTINATION</span>
                         <div className="h-[3px] bg-orange-500 w-[2rem]"></div>
                         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, incidunt.</span>
                     </div>
                 </div>
-                <div className="flex card h-[20rem] w-[15rem] shadow-md rounded-md">
+                <div className="flex card h-[20rem] w-[20rem] p-4 shadow-md rounded-md">
                     <div className="flex flex-col mt-auto text-white justify-center items-center flex-1 hover:h-[15rem] duration-150">
                         <span>DESTINATION</span>
                         <div className="h-[3px] bg-orange-500 w-[2rem]"></div>
                         <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, incidunt.</span>
                     </div>
                 </div>
-                <div className="flex card h-[20rem] w-[15rem] shadow-md rounded-md">
+                <div className="flex card h-[20rem] w-[20rem] p-4 shadow-md rounded-md">
                     <div className="flex flex-col mt-auto text-white justify-center items-center flex-1 hover:h-[15rem] duration-150">
                         <span>DESTINATION</span>
                         <div className="h-[3px] bg-orange-500 w-[2rem]"></div>
